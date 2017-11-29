c22f723732f0336adf47a7b8ca56f214922e2707
skmtpalm <hachihachi.8og@gmail.com>
2017-11-29 11:37:02 +0900

    task list styling

--- app/javascript/javascripts/application.js
+++ app/javascript/javascripts/application.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import $ from 'jquery';
+
+(($) => {
+  $('.task-text-toggle-nav').on('click', function() {
+    const targetTaskId = $(this).data('target-task-id')
+
+    $(`#task-text-${targetTaskId}`).toggleClass('is-active');
+  })
+})($)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(title: 'Hello Toodoly', description: 'This is Smaple task', due: 1.days.since)
+Task.create!(title: 'Buy milk!', description: 'This is Smaple task', due: 2.days.since)
